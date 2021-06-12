358f17474e5cc9d5d433fe21567340e35b278249
muratsivri <75843728+muratsivri@users.noreply.github.com>
2021-06-12 22:57:29 +0300

    Under development...

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.Concrete.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,26 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car,NorthwindContext>, ICarDal
     {
-        
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from c in context.Cars
+                             join col in context.Colors
+                             on c.ColorId equals col.Id
+                             join b in context.Brands
+                             on c.BrandId equals b.Id
+                             select new CarDetailDto
+                             {
+
+                                 CarName = c.CarName,
+                                 ColorName = col.ColorName,
+                                 BrandName = b.BrandName,
+                                 DailyPrice = c.DailyPrice
+                             };
+                return result.ToList();
+            }
+        }
     }
+   
 }
