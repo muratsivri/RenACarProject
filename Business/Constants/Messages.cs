77f7edc08a2173a1d6df2a7a4e9bc5d2fe5421fd
muratsivri <75843728+muratsivri@users.noreply.github.com>
2021-06-13 17:20:12 +0300

    Under development...

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,5 +12,19 @@ namespace Business.Constants
         public static string ProductDeleted = "Ürün silindi";
         public static string MaintenanceTime = "Bakım zamanı";
         public static string ProductListed = "Ürünler listelendi";
+        public static string RentalAdded = "Araç eklendi";
+        public static string RentalDeleted = "Araç silindi";
+        public static string RentalUpdated = "Araç güncellendi";
+        public static string RentalInvalid = "Araç teslim edilmedi";
+        public static string RentalsListed = "Araçlar listelendi";
+        public static string UserAdded = "Kulllanıcı eklendi";
+        public static string UserDeleted = "Kulllanıcı silindi";
+        public static string UserUpdated = "Kulllanıcı güncellendi";
+        public static string UsersListed = "Kullanıcılar listelendi";
+        public static string CustomerAdded = "Müşteri eklendi";
+        public static string CustomerDeleted = "Müşteri silindi";
+        public static string CustomerUpdated = "Müşteri güncellendi";
+        public static string CustomersListed = "Müşteri listelendi";
+
     }
 }
